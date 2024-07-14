f885f940c8447fb2e78f7ec7fcc7ee4ab968630d
weeeeei <weeeeei@duck.com>
2024-07-14 09:17:30 +0800

    Base

---
+++ lua/plugins/init.lua
@@ -0,0 +1,30 @@
+require("plugins/lazy-pm")
+
+require("lazy").setup({
+	spec = {
+		{
+			"neovim/nvim-lspconfig",
+			init = function()
+				require("../configs/lspconfig")
+			end,
+		},
+		{
+			"echasnovski/mini.nvim",
+			init = function()
+				require("mini.completion").setup()
+			end,
+		},
+		{
+			"williamboman/mason.nvim",
+			init = function()
+				require("mason").setup()
+			end,
+		},
+		{
+			"mhartington/formatter.nvim",
+			init = function()
+				require("../configs/formatter")
+			end,
+		},
+	},
+})
